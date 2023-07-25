0eebd830a075a101205e86235041e5f40a7e9247
1743998121@qq.com <1743998121@qq.com>
2023-07-25 14:32:08 +0800

    d24

--- common/mock.py
+++ common/mock.py
@@ -23,12 +23,30 @@ class Mock:
 
         return strs
 
+    @staticmethod
+    def faker_pystr():
+        # max_chars最大位数，min_chars最小，numerify=True表示生成的字符串只包含数字字符。
+        faker = Faker()
+
+        strs = faker.pystr(max_chars=5)
+
+        return strs
+
+
+    @staticmethod
+    def faker_num():
+
+        faker = Faker()
+
+        st = faker.random_number(digits=5)
+
+        return st
 
 if __name__ == '__main__':
 
     mock = Mock()
 
-    a2 = mock.faker_data()
+    a2 = mock.faker_pystr()
 
     print(a2)
 
