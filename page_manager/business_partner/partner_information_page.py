8429fc78d140b1762f7ede37386892033c5700cd
lcygggggjv <1743998121@qq.com>
2023-07-30 23:08:23 +0800

    66663

--- page_manager/business_partner/partner_information_page.py
+++ page_manager/business_partner/partner_information_page.py
@@ -8,8 +8,8 @@ class PartnerPage(BasePage):
 
     mock = Mock()
 
-    def create_tax_rate(self):
-        """新增税率"""
+    def create_partner(self):
+        """新增业务伙伴"""
 
         time.sleep(0.5)
         if self.is_el_present(("xpath", "//h6[text()='暂无数据']")):
@@ -17,7 +17,8 @@ class PartnerPage(BasePage):
             self.driver.find_element("xpath", '//input[@name="no"][@placeholder="请输入"]').send_keys('99999')
         elif self.is_el_present(('xpath', '//td[@label="业务伙伴编码"]//a[text()="99999"]')):
             self.driver.find_element("xpath", "//button[text()='新增业务伙伴信息']").click()
-            self.driver.find_element("xpath", '//input[@name="rate"]').send_keys(self.mock.faker_pystr())
+            self.driver.find_element("xpath", '//input[@name="no"][@placeholder="请输入"]')\
+                .send_keys(self.mock.faker_pystr())
         else:
             self.driver.find_element("xpath", "//button[text()='新增业务伙伴信息']").click()
             self.driver.find_element("xpath", '//input[@name="no"][@placeholder="请输入"]').send_keys('99999')
@@ -30,13 +31,11 @@ class PartnerPage(BasePage):
         self.driver.find_element("xpath", '(//button[@role="gridcell"][text()="1"])[1]').click()
         self.driver.find_element("xpath", '(//button[@role="gridcell"][text()="28"])[2]').click()
         self.driver.find_element("xpath", '//div[@name="defaultCurrency"]//input[@role="combobox"]').click()
-
         self.driver.find_element("xpath", '//li[@data-option-index="0"]').click()
         self.driver.find_element("xpath", '//input[@name="creditCode"]').send_keys(self.mock.faker_num_18())
         self.driver.find_element("xpath", '//input[@name="licenseCode"]').send_keys(self.mock.faker_num_18())
         self.driver.find_element("xpath", '//div[@name="companyAddr"]//input[@role="combobox"]').click()
         self.driver.find_element("xpath", '//span[text()="中国香港"]').click()
-
         self.driver.find_element("xpath", '//input[@name="address"]').send_keys(self.mock.faker_pystr())
         self.driver.find_element("xpath", '//input[@name="remark"]').send_keys(self.mock.faker_pystr())
         self.driver.find_element("xpath", '//input[@name="contactList.0.name"]').send_keys(self.mock.faker_pystr())
@@ -48,92 +47,150 @@ class PartnerPage(BasePage):
         assert_info = self.get_alert(("xpath", "//div[text()='新增成功']"))
         return assert_info
 
-    def create_tax_rate_required(self):
-        """新增税率必填"""
+    def create_partner_required(self):
+        """新增业务伙伴必填"""
 
-        self.driver.find_element("xpath", "//button[text()='新增税率']").click()
+        self.driver.find_element("xpath", "//button[text()='新增业务伙伴信息']").click()
         self.driver.find_element("xpath", "//button[text()='确定']").click()
         assert_info = self.get_alert(("xpath", "//div[text()='请填写该必填项']"))
         return assert_info
 
-    def create_tax_negative_num(self):
-        """新增税率 负数校验"""
+    def create_partner_code_length(self):
+        """新增业务伙伴编码长度 校验"""
 
-        self.driver.find_element("xpath", '//input[@name="rate"]').send_keys(-1)
-        assert_info = self.get_alert(("xpath", "//div[text()='请输入0~99的整数']"))
+        self.driver.find_element("xpath", '//input[@name="no"][@placeholder="请输入"]')\
+            .send_keys(self.mock.faker_pystr_21())
+        assert_info = self.get_alert(("xpath", "//div[text()='请输入20个字以内的内容']"))
         return assert_info
 
-    def create_tax_most_num(self):
-        """新增税率超过100校验 """
+    def create_partner_name_length(self):
+        """新增业务伙伴名称 长度 校验"""
 
-        self.double_click_delete('//input[@name="rate"]')
-        # self.driver.find_element("xpath", '//input[@name="rate"]').clear()
-        self.driver.find_element("xpath", '//input[@name="rate"]').send_keys(101)
-        assert_info = self.get_alert(("xpath", "//div[text()='请输入0~99的整数']"))
+        # self.double_click_delete('//input[@name="rate"]')
+        self.driver.find_element("xpath", '//input[@name="no"][@placeholder="请输入"]').clear()
+        self.driver.find_element("xpath", '//input[@name="name"][@placeholder="请输入"]')\
+            .send_keys(self.mock.faker_pystr_21())
+        assert_info = self.get_alert(("xpath", "//div[text()='请输入20个字以内的内容']"))
         return assert_info
 
-    def create_tax_str(self):
-        """新增税率 输入字符串等校验"""
+    def create_partner_jc_length(self):
+        """新增业务伙伴简称 长度 校验"""
 
-        self.double_click_delete('//input[@name="rate"]')
-        self.driver.find_element("xpath", '//input[@name="rate"]').send_keys('xx')
-        assert_info = self.get_alert(("xpath", "//div[text()='请填写该必填项']"))
+        self.driver.find_element("xpath", '//input[@name="name"][@placeholder="请输入"]').clear()
+        self.driver.find_element("xpath", '//input[@name="abbreviation"][@placeholder="请输入"]')\
+            .send_keys(self.mock.faker_pystr_21())
+        assert_info = self.get_alert(("xpath", "//div[text()='请输入10个字以内的内容']"))
         return assert_info
 
-    def create_tax_float(self):
-        """新增税率 小数等校验"""
+    def create_community_code_length(self):
+        """新增业务伙伴信用代码 长度校验"""
 
-        self.double_click_delete('//input[@name="rate"]')
-        self.driver.find_element("xpath", '//input[@name="rate"]').send_keys(1.53)
-        assert_info = self.get_alert(("xpath", "//div[text()='请输入0~99的整数']"))
+        self.driver.find_element("xpath", '//input[@name="abbreviation"][@placeholder="请输入"]').clear()
+        self.driver.find_element("xpath", '//input[@name="creditCode"]').send_keys(1)
+        assert_info1 = self.get_alert(("xpath", "//div[text()='请输入18位统一社会信用代码']"))
+        self.driver.find_element("xpath", '//input[@name="creditCode"]').clear()
+        self.driver.find_element("xpath", '//input[@name="creditCode"]').send_keys(self.mock.faker_pystr_21())
+        assert_info2 = self.get_alert(("xpath", "//div[text()='请输入18位统一社会信用代码']"))
+        return assert_info1, assert_info2
+
+    def create_business_license_length(self):
+        """新增业务伙伴营业执照 长度校验"""
+
+        self.driver.find_element("xpath", '//input[@name="creditCode"]').clear()
+        self.driver.find_element("xpath", '//input[@name="licenseCode"]').send_keys(1)
+        assert_info1 = self.get_alert(("xpath", "//div[text()='请输入15位或18位营业执照编号']"))
+        self.driver.find_element("xpath", '//input[@name="licenseCode"]').clear()
+        self.driver.find_element("xpath", '//input[@name="licenseCode"]').send_keys(self.mock.faker_pystr_21())
+        assert_info2 = self.get_alert(("xpath", "//div[text()='请输入15位或18位营业执照编号']"))
+        return assert_info1, assert_info2
+
+    def create_address_length(self):
+        """新增业务伙伴地址 长度校验"""
+
+        self.driver.find_element("xpath", '//input[@name="licenseCode"]').clear()
+        self.driver.find_element("xpath", '//input[@name="address"]').send_keys(self.mock.faker_pystr_21())
+        assert_info = self.get_alert(("xpath", "//div[text()='请输入20个字以内的内容']"))
         return assert_info
 
-    def create_tax_uniqueness(self):
-        """新增税率 唯一性"""
+    def create_remark1_length(self):
+        """新增业务伙伴备注1 长度校验"""
 
-        self.double_click_delete('//input[@name="rate"]')
-        self.driver.find_element("xpath", '//input[@name="rate"]').send_keys(9)
-        assert_info = self.get_alert(("xpath", "//div[text()='该税率已存在，请重新输入']"))
+        self.driver.find_element("xpath", '//input[@name="address"]').clear()
+        self.driver.find_element("xpath", '//input[@name="remark"]').send_keys(self.mock.faker_data_61())
+        assert_info = self.get_alert(("xpath", "//div[text()='请输入60个字以内的内容']"))
         return assert_info
 
-    def create_tax_rate_two(self):
-        """新增税率2"""
+    def create_contacts_length(self):
+        """新增业务伙伴联系人 长度校验"""
 
-        self.double_click_delete('//input[@name="rate"]')
-        self.driver.find_element("xpath", '//input[@name="rate"]').send_keys(self.mock.random_int())
+        self.driver.find_element("xpath", '//input[@name="remark"]').clear()
+        self.driver.find_element("xpath", '//input[@name="contactList.0.name"]').send_keys(self.mock.faker_pystr_21())
+        assert_info = self.get_alert(("xpath", "//div[text()='请输入20个字以内的内容']"))
+        return assert_info
+
+    def create_fixed_phone_length(self):
+        """新增业务伙伴固定电话 长度校验"""
+
+        self.driver.find_element("xpath", '//input[@name="contactList.0.name"]').clear()
+        self.driver.find_element("xpath", '//input[@name="contactList.0.fixedPhone"]')\
+            .send_keys(self.mock.faker_pystr_21())
+        assert_info = self.get_alert(("xpath", "//div[text()='请输入8个字以内的内容']"))
+        return assert_info
+
+    def create_position_length(self):
+        """新增业务伙伴职位 长度校验"""
+
+        self.driver.find_element("xpath", '//input[@name="contactList.0.fixedPhone"]').clear()
+        self.driver.find_element("xpath", '//input[@name="contactList.0.position"]')\
+            .send_keys(self.mock.faker_pystr_21())
+        assert_info = self.get_alert(("xpath", "//div[text()='请输入10个字以内的内容']"))
+        return assert_info
+
+    def create_remark2_length(self):
+        """新增业务伙伴备注1 长度校验"""
+
+        self.driver.find_element("xpath", '//input[@name="contactList.0.position"]').clear()
+        self.driver.find_element("xpath", '//input[@name="contactList.0.remark"]').send_keys(self.mock.faker_data_61())
+        assert_info = self.get_alert(("xpath", "//div[text()='请输入60个字以内的内容']"))
+        return assert_info
+
+    def create_phone_format(self):
+        """新增业务伙伴手机格式 校验"""
+
+        self.driver.find_element("xpath", '//input[@name="contactList.0.remark"]').clear()
+        self.driver.find_element("xpath", '//input[@name="contactList.0.phone"]').send_keys(1)
+        assert_info1 = self.get_alert(("xpath", "//div[text()='手机号格式错误，请重新输入']"))
+        self.driver.find_element("xpath", '//input[@name="contactList.0.phone"]').clear()
+        self.driver.find_element("xpath", '//input[@name="contactList.0.phone"]').send_keys(self.mock.faker_pystr_21())
+        assert_info2 = self.get_alert(("xpath", "//div[text()='手机号格式错误，请重新输入']"))
+        self.driver.find_element("xpath", '//input[@name="contactList.0.phone"]').clear()
+        return assert_info1, assert_info2
+
+    def create_partner_two(self):
+        """新增业务伙伴2"""
+
+        self.driver.find_element("xpath", '//input[@name="no"][@placeholder="请输入"]').send_keys(self.mock.faker_pystr())
+        self.driver.find_element("xpath", '(//div[@name="partnerType"]//input[@type="checkbox"])[1]').click()
+        self.driver.find_element("xpath", '//input[@name="name"][@placeholder="请输入"]')\
+            .send_keys(self.mock.faker_pystr())
+        self.driver.find_element("xpath", '//input[@name="abbreviation"][@placeholder="请输入"]')\
+            .send_keys(self.mock.faker_pystr())
+        self.driver.find_element("xpath", '//input[@name="expectedAt"]').click()
+        self.driver.find_element("xpath", '(//button[@role="gridcell"][text()="1"])[1]').click()
+        self.driver.find_element("xpath", '(//button[@role="gridcell"][text()="28"])[2]').click()
+        self.driver.find_element("xpath", '//div[@name="defaultCurrency"]//input[@role="combobox"]').click()
+        self.driver.find_element("xpath", '//li[@data-option-index="0"]').click()
+        self.driver.find_element("xpath", '//input[@name="creditCode"]').send_keys(self.mock.faker_num_18())
+        self.driver.find_element("xpath", '//input[@name="licenseCode"]').send_keys(self.mock.faker_num_18())
+        self.driver.find_element("xpath", '//div[@name="companyAddr"]//input[@role="combobox"]').click()
+        self.driver.find_element("xpath", '//span[text()="中国香港"]').click()
+        self.driver.find_element("xpath", '//input[@name="address"]').send_keys(self.mock.faker_pystr())
+        self.driver.find_element("xpath", '//input[@name="remark"]').send_keys(self.mock.faker_pystr())
+        self.driver.find_element("xpath", '//input[@name="contactList.0.name"]').send_keys(self.mock.faker_pystr())
+        self.driver.find_element("xpath", '//input[@name="contactList.0.phone"]').send_keys(self.mock.rand_phone_num())
+        self.driver.find_element("xpath", '//input[@name="contactList.0.fixedPhone"]').send_keys(self.mock.ran_phone2())
+        self.driver.find_element("xpath", '//input[@name="contactList.0.position"]').send_keys(self.mock.faker_pystr())
+        self.driver.find_element("xpath", '//input[@name="contactList.0.remark"]').send_keys(self.mock.faker_pystr())
         self.driver.find_element("xpath", "//button[text()='确定']").click()
         assert_info = self.get_alert(("xpath", "//div[text()='新增成功']"))
-        return assert_info
-
-    def open_checkbox(self):
-        """打开税率开关"""
-
-        time.sleep(1.8)
-        self.driver.find_element("xpath", '(//td[@label="是否生效"]//span)[1]').click()
-        self.driver.find_element("xpath", '//button[text()="生效"]').click()
-        assert_info = self.get_alert(("xpath", "//div[text()='生效成功']"))
-        return assert_info
-
-    def search_tax_rate(self):
-        """精确搜索税率"""
-
-        self.driver.find_element('xpath', '//input[@name="search"]').send_keys(9)
-        self.driver.find_element('xpath', '//button[@aria-label="查询"]').click()
-        assert_info = self.get_alert(('xpath', "//td[text()='9'][@label='税率（%）']"))
-        return assert_info
-
-    def resetting_search(self):
-        """重置搜索"""
-
-        self.driver.find_element("xpath", '//button[@aria-label="重置"]').click()
-        el = self.driver.find_element('xpath', '//input[@name="search"]')
-        assert_info = el.get_attribute('value')
-        return assert_info
-
-    def delete_tax_rate(self):
-        """删除税率"""
-
-        self.driver.find_element("xpath", '(//span[@aria-label="删除"])[1]').click()
-        self.driver.find_element("xpath", "//button[text()='删除']").click()
-        assert_info = self.get_alert(("xpath", "//div[text()='删除成功']"))
         return assert_info
---
+++ testcase/business_partner_case/test_partner_case.py
@@ -0,0 +1,122 @@
+import allure
+import pytest
+from page_manager.business_partner.partner_information_page import PartnerPage
+
+
+class TestPartnerPage:
+
+    partner = None
+
+    @classmethod
+    def setup_class(cls):
+
+        cls.partner = PartnerPage()
+        cls.partner.goto_partner_page()
+
+    @pytest.mark.P0
+    @allure.testcase(url="", name='新增业务伙伴')
+    def test_create_partner(self):
+
+        assert_info = self.partner.create_partner()
+        self.partner.assert_allure_screenshot(assert_info, "新增成功")
+
+    @pytest.mark.P2
+    @allure.testcase(url="", name='新增必填')
+    def test_create_partner_required(self):
+
+        assert_info = self.partner.create_partner_required()
+        self.partner.assert_allure_screenshot(assert_info, "请填写该必填项")
+
+    @pytest.mark.P2
+    @allure.testcase(url="", name='新增业务伙伴编码长度校验')
+    def test_create_partner_code_length(self):
+
+        assert_info = self.partner.create_partner_code_length()
+        self.partner.assert_allure_screenshot(assert_info, "请输入20个字以内的内容")
+
+    @pytest.mark.P2
+    @allure.testcase(url="", name='新增业务伙伴名称长度校验')
+    def test_create_partner_name_length(self):
+
+        assert_info = self.partner.create_partner_name_length()
+        self.partner.assert_allure_screenshot(assert_info, "请输入20个字以内的内容")
+
+    @pytest.mark.P2
+    @allure.testcase(url="", name='新增业务伙伴简称长度校验')
+    def test_create_partner_jc_length(self):
+
+        assert_info = self.partner.create_partner_jc_length()
+        self.partner.assert_allure_screenshot(assert_info, "请输入10个字以内的内容")
+
+    @pytest.mark.P2
+    @allure.testcase(url="", name='新增业务伙伴信用代码 长度校验')
+    def test_create_community_code_length(self):
+
+        assert_info = self.partner.create_community_code_length()
+        self.partner.assert_allure_screenshot(assert_info[0], "请输入18位统一社会信用代码")
+        self.partner.assert_allure_screenshot(assert_info[1], "请输入18位统一社会信用代码")
+
+    @pytest.mark.P2
+    @allure.testcase(url="", name='新增业务伙伴营业执照 长度校验')
+    def test_create_business_license_length(self):
+
+        assert_info = self.partner.create_business_license_length()
+        self.partner.assert_allure_screenshot(assert_info[0], "请输入15位或18位营业执照编号")
+        self.partner.assert_allure_screenshot(assert_info[1], "请输入15位或18位营业执照编号")
+
+    @pytest.mark.P2
+    @allure.testcase(url="", name='新增地址长度校验')
+    def test_create_address_length(self):
+
+        assert_info = self.partner.create_address_length()
+        self.partner.assert_allure_screenshot(assert_info, "请输入20个字以内的内容")
+
+    @pytest.mark.P2
+    @allure.testcase(url="", name='新增备注1长度校验')
+    def test_create_remark1_length(self):
+
+        assert_info = self.partner.create_remark1_length()
+        self.partner.assert_allure_screenshot(assert_info, "请输入60个字以内的内容")
+
+    @pytest.mark.P2
+    @allure.testcase(url="", name='新增业务伙伴联系人 长度校验')
+    def test_create_contacts_length(self):
+
+        assert_info = self.partner.create_contacts_length()
+        self.partner.assert_allure_screenshot(assert_info, '请输入20个字以内的内容')
+
+    @pytest.mark.P2
+    @allure.testcase(url="", name='新增业务伙伴固定电话 长度校验')
+    def test_create_fixed_phone_length(self):
+
+        assert_info = self.partner.create_fixed_phone_length()
+        self.partner.assert_allure_screenshot(assert_info, '请输入8个字以内的内容')
+
+    @pytest.mark.P2
+    @allure.testcase(url="", name='新增业务伙伴职位 长度校验')
+    def test_create_position_length(self):
+
+        assert_info = self.partner.create_position_length()
+        self.partner.assert_allure_screenshot(assert_info, '请输入10个字以内的内容')
+
+    @pytest.mark.P2
+    @allure.testcase(url="", name='新增备注2, 长度校验')
+    def test_create_remark2_length(self):
+
+        assert_info = self.partner.create_remark2_length()
+        self.partner.assert_allure_screenshot(assert_info, "请输入60个字以内的内容")
+
+    @pytest.mark.P2
+    @allure.testcase(url="", name='新增业务伙伴手机格式 校验')
+    def test_create_phone_format(self):
+
+        assert_info = self.partner.create_phone_format()
+        self.partner.assert_allure_screenshot(assert_info[0], "手机号格式错误，请重新输入")
+        self.partner.assert_allure_screenshot(assert_info[1], "手机号格式错误，请重新输入")
+
+    @pytest.mark.P2
+    @allure.testcase(url="", name='新增业务伙伴2')
+    def test_create_partner_two(self):
+
+        assert_info = self.partner.create_partner_two()
+        self.partner.assert_allure_screenshot(assert_info, "新增成功")
